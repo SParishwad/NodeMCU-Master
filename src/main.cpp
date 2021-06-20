91996604d6c629c57cf4bfd7ca741461b17ea52d
SParishwad <36662803+SParishwad@users.noreply.github.com>
2021-06-20 19:34:06 +0200

    Attempted 2 way communication

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
  * For the NodeMCU to be operate as a master with the ESP32 as slave
  * Contained Blocks
  *    - Websocket Server for Android (A good explanation of the WebSocket server can be found here. http://www.martyncurrey.com/esp8266-and-the-arduino-ide-part-9-websockets/)
- *    - Radio Transmission (nRF24L01)
+ *    - Radio Transmission (nRF24L01) https://forum.arduino.cc/t/simple-nrf24l01-2-4ghz-transceiver-demo/405123/2
+ *       - ctrlData is the data to control the servos
+ *       - imuData is the data received from the IMUs
  */
 
 #include <Arduino.h>
@@ -24,10 +26,10 @@
 #include <RF24.h>
 #define CE_PIN D8
 #define CSN_PIN D4
-int ackData[2] = {-1, -1}; // to hold the two values coming from the slave
-const byte addresses[][6] = {"00001", "00002"};
-//const byte addresses[6] = "00001";
-char dataReceived[32] = {0};    // this must match dataToSend in the TX
+//const byte addresses[][6] = {"00001", "00002"};
+const byte addresses[6] = "00001";
+//char imuData[19] = {0};    // IMU Data received from the Slave
+int imuData[20] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}; // to hold the two values coming from the slave
 bool newData = false;
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 /*******************************************************************/
@@ -41,8 +43,8 @@ ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 // num is the current client/connection number/ID.
-// payload is the received data. This is a pointer not a char or char array.
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+// ctrlData is the received data. This is a pointer not a char or char array.
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *ctrlData, size_t length)
 {
 
     switch (type)
@@ -53,7 +55,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
     case WStype_CONNECTED:
     {
         IPAddress ip = webSocket.remoteIP(num);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], ctrlData);
 
         // send message to client
         webSocket.sendTXT(num, "Connected");
@@ -61,9 +63,25 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
     break;
     case WStype_TEXT:
         IPAddress ip = webSocket.remoteIP(num);
-        //Serial.printf("%s?\n", payload);
+        bool rslt;
+        rslt = radio.write( ctrlData, length );
+        Serial.print("Data Sent ");
+        Serial.printf("%s", ctrlData);
+        if(rslt){               // Everything in this bracket is part of a two way radio
+            if ( radio.isAckPayloadAvailable() ) {
+                radio.read(&imuData, sizeof(imuData));
+                newData = true;
+            }
+            else {
+                Serial.println("  Acknowledge but no data ");
+            }
+        }
+        else{Serial.println("Tx Failed");}
+
+
+        //Serial.printf("%s?\n", ctrlData);
         /*for (int i = 0; i <= length; i++){
-                Serial.println((char)payload[i]);
+                Serial.println((char)ctrlData[i]);
             }*/
 
         /**
@@ -72,26 +90,17 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
              * http://www.cplusplus.com/reference/cstdlib/strtof/
              * https://tttapa.github.io/ESP8266/Chap14%20-%20WebSocket.html
              */
-        //radio.write( &payload, length );
-        //radio.write( (uint8_t *)payload, length );
-        //radio.write(payload, length);
-        //webSocket.sendTXT(num, payload,sizeof(payload),false);
-        radio.stopListening();
-        radio.write(payload, length);
+        //radio.write( &ctrlData, length );
+        //radio.write( (uint8_t *)ctrlData, length );
+        //radio.write(ctrlData, length);
+        //webSocket.sendTXT(num, ctrlData,sizeof(ctrlData),false);
+        
+        // Commenting out to try 2 way communication.
+        /* radio.stopListening();
+        radio.write(ctrlData, length);
         Serial.print("Data Sent ");
-        Serial.printf("%s", payload);
-        Serial.println();
-        /*if(rslt){               // Everything in this bracket is part of a two way radio
-                if ( radio.isAckPayloadAvailable() ) {
-                    radio.read(&ackData, sizeof(ackData));
-                    newData = true;
-                }
-                else {
-                    Serial.println("  Acknowledge but no data ");
-                }
-                //updateMessage();
-            }
-            else{Serial.println("Tx Failed");}*/
+        Serial.printf("%s", ctrlData);
+        Serial.println(); */
         break;
     }
 }
@@ -131,15 +140,32 @@ void setup()
     //Radio Communication (nRF24L01+LA+PNA)
     Serial.println("Radio Communnication Starting...");
     radio.begin();
-    radio.openWritingPipe(addresses[0]); // 00001    // This is checked
+    radio.setDataRate( RF24_250KBPS );
+    radio.enableAckPayload();
+    radio.setRetries(5,5); // delay, count
+    radio.openWritingPipe(addresses);
+    /* radio.openWritingPipe(addresses[0]); // 00001    // This is checked
     radio.openReadingPipe(1, addresses[1]); // 00002   // This is checked
-    radio.setPALevel(RF24_PA_MIN);
+    radio.setPALevel(RF24_PA_MIN);*/
     delay(1000);
     Serial.println("Radio Communication Started!");
 
     //Add service to MDNS
     MDNS.addService("http", "tcp", 80);
     MDNS.addService("ws", "tcp", 81);
+}
+
+void showData() {
+    if (newData == true) {
+        Serial.print("  Acknowledge data ");
+        Serial.println(imuData[0]);
+        Serial.println(imuData[1]);
+        Serial.println(imuData[2]);
+        Serial.println(imuData[3]);
+        Serial.println(imuData[4]);
+        Serial.println();
+        newData = false;
+    }
 }
 
 void loop()
@@ -149,10 +175,11 @@ void loop()
     //delay(1000);
     webSocket.loop();
     server.handleClient();
-    radio.startListening();
+    /* radio.startListening();
     if (radio.available()){
         radio.read(&dataReceived, sizeof(dataReceived));
         Serial.print(dataReceived);
-    }
+    } */
     //if(newData){webSocket.broadcastTXT();}     // BroadcastTXT is used to transmit data to all the WebSocket Clients.
+    showData();
 }
